eae37dc6e96fc70ece19eed40505418b6b8184e3
정은진 <jin5718@naver.com>
2024-03-18 17:16:51 +0900

    Feat: Feed, Coordi API 추가 및 수정

--- backend/src/main/java/com/d111/backend/controller/feed/FeedController.java
+++ backend/src/main/java/com/d111/backend/controller/feed/FeedController.java
@@ -1,15 +1,15 @@
 package com.d111.backend.controller.feed;
 
-import com.d111.backend.entity.coordi.Coordi;
+import com.d111.backend.dto.feed.reponse.FeedCreateResponse;
+import com.d111.backend.dto.feed.reponse.FeedDeleteResponse;
+import com.d111.backend.dto.feed.reponse.FeedListReadResponse;
+import com.d111.backend.dto.feed.reponse.FeedReadResponse;
+import com.d111.backend.dto.feed.request.FeedCoordiCreateRequest;
 import com.d111.backend.service.feed.FeedService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Feed", description = "Feed API")
 @RequestMapping("/api/feed")
@@ -19,13 +19,34 @@ public class FeedController {
 
     private final FeedService feedService;
 
+    // 피드 전체 조회
     @GetMapping
-    public ResponseEntity<List<Coordi>> readCoordi() {
-        return feedService.read();
+    public ResponseEntity<FeedListReadResponse> readFeedList() {
+        return feedService.readList();
     }
-}
 
-//    @GetMapping("/{id}")
-//    public ResponseEntity<FeedReadResponse> feedRead(@PathVariable String id) {
-//        return feedService.read(id);
+    // 피드 및 코디 생성
+    @PostMapping("/create")
+    public ResponseEntity<FeedCreateResponse> createFeedCoordi(@RequestBody FeedCoordiCreateRequest request) {
+        return feedService.create(request.getFeedCreateRequest(), request.getCoordiCreateRequest());
+    }
+
+
+    // 피드 개별 조회
+//    @GetMapping("/{feedId}")
+//    public ResponseEntity<FeedReadResponse> readFeed(@PathVariable Long feedId){
+//        return feedService.read(feedId);
 //    }
+
+    @GetMapping("/{coordiId}")
+    public ResponseEntity<FeedReadResponse> readFeed(@PathVariable String coordiId){
+        return feedService.read(coordiId);
+    }
+
+
+    @PostMapping("/{coordiId}")
+    public ResponseEntity<FeedDeleteResponse> deleteFeed(@PathVariable String coordiId){
+        return feedService.delete(coordiId);
+    }
+
+}
--- backend/src/main/java/com/d111/backend/dto/coordi/request/CoordiCreateRequest.java
+++ backend/src/main/java/com/d111/backend/dto/coordi/request/CoordiCreateRequest.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CoordiCreateRequest {
 
+
     private String head;
 
     private String upperBody;
