88b79ff3c36712794231a7c4826e083f0586aaea
정은진 <jin5718@naver.com>
2024-03-21 15:18:07 +0900

    Feat: Feed 반환 형태 수정

--- backend/src/main/java/com/d111/backend/dto/feed/reponse/FeedListReadResponse.java
+++ backend/src/main/java/com/d111/backend/dto/feed/reponse/FeedListReadResponse.java
@@ -30,7 +30,7 @@ public class FeedListReadResponse {
                 .map(feed -> {
                     CoordiContainer coordiContainer = createMongoContainer(feed.getCoordiId(), mongoCoordiRepository);
 
-                    return new FeedInfo(feed.getId(), feed.getFeedTitle(), feed.getFeedThumbnail(), coordiContainer);
+                    return new FeedInfo(feed.getId(), feed.getUserId().getId(), feed.getFeedTitle(), feed.getFeedThumbnail(), coordiContainer);
                 })
                 .collect(Collectors.toList());
 
@@ -45,17 +45,18 @@ public class FeedListReadResponse {
     public static class FeedInfo {
         @Schema(description = "피드 번호", example = "1")
         private Long feedId;
+        @Schema(description = "유저 번호", example = "1")
+        private Long userId;
         @Schema(description = "피드 제목", example = "멋있는 코디")
         private String feedTitle;
-//        @Schema(description = "피드 내용", example = "님들도 이렇게 입으세요")
-//        private String feedContent;
         @Schema(description = "피드 내용", example = "example.com")
         private String feedThumbnail;
 
         private CoordiContainer coordiContainer;
 
-        public FeedInfo(Long feedId, String feedTitle, String feedThumbnail, CoordiContainer coordiContainer) {
+        public FeedInfo(Long feedId, Long userId, String feedTitle, String feedThumbnail, CoordiContainer coordiContainer) {
             this.feedId = feedId;
+            this.userId = userId;
             this.feedTitle = feedTitle;
             this.feedThumbnail = feedThumbnail;
             this.coordiContainer = coordiContainer;
