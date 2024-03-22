de24bdf686eb9e587837a705fd8c3b0cca0648ae
오승현 <ohsh9529@gmail.com>
2024-03-22 09:56:28 +0900

    feeddetail

--- frontend/src/pages/FeedDetail.tsx
+++ frontend/src/pages/FeedDetail.tsx
@@ -12,15 +12,47 @@ const FeedDetail = () => {
         <div>
           <div className='flex justify-center'>ITEMS</div>
           <div className='flex justify-center'>
-            <div className="hero min-h-screen max-w-screen-xl bg-base-200 flex flex-col">
+            <div className="hero max-h-screen-xl max-w-screen-xl bg-base-200 flex flex-col">
               <div className="hero-content flex-col lg:flex-row">
                 <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
               </div>
               <div>
-                <div>코디이름</div>
-                <div>최초등록자 : 유저이름</div>
+                <div>feedTitle</div>
+                <div>최초등록자 : userid</div>
                 <div>예상 가격 : 00원</div>
                 <button className="btn btn-outline">피팅 해보기</button>
+              </div>
+            </div>
+          </div>
+          <div className='flex justify-center items-center'>
+            <div className="hero max-h-screen-xl max-w-screen-xl bg-base-200 flex flex-grow items-start mt-5 ">
+              <div className="avatar">
+                <div className="w-10 h-10 rounded-full">
+                  <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+              </div>
+              <div className='flex justify-between flex-grow'>
+                <div>
+                  <div className='ml-3'>userid</div>
+                  <div className='ml-3'>feedcontent</div>
+                </div>
+                <div className='mr-3 flex justify-end'>feedCreatedDate</div>
+              </div>
+            </div>
+          </div>
+          <div>Comments</div>
+          <div className='flex justify-center items-center'>
+            <div className="hero max-h-screen-xl max-w-screen-xl bg-base-200 flex flex-grow items-start mt-5 ">
+              <div className="avatar">
+                <div className="w-10 h-10 rounded-full">
+                  <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+              </div>
+              <div className='flex justify-between flex-grow'>
+                <div>
+                  <div className='ml-3'>userid</div>
+                  <div className='ml-3'>feedcontent</div>
+                </div>
               </div>
             </div>
           </div>
