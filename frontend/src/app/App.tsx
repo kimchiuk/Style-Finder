d533ca6874a40b8b4ad965c82b27b12d90b32d89
오승현 <ohsh9529@gmail.com>
2024-03-25 14:58:26 +0900

    feat : feed수정

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,6 +3,7 @@ import LoginPage from '../pages/SignIn';
 import SignUpPage from '../pages/SignUp';
 import CoordiPage from '../pages/Coordi';
 import FeedPage from '../pages/Feed';
+import FeedDetail from '../pages/FeedDetail'
 import AnalysisPage from '../pages/Analysis';
 import MyPage from '../pages/My';
 import RecommendationPage from '../pages/Recommendation';
@@ -18,6 +19,7 @@ const Router = () => {
         <Route path="/signup" element={<SignUpPage />}></Route>
         <Route path="/coordi" element={<CoordiPage />}></Route>
         <Route path="/feed" element={<FeedPage />}></Route>
+        <Route path="/feeddetail" element={<FeedDetail />}></Route>
         <Route path="/analysis/*" element={<AnalysisPage />}>
           <Route path="my" element={<MyPage />}></Route>
           <Route path="recommendation" element={<RecommendationPage />}></Route>
