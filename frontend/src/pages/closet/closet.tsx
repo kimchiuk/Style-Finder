48d796e7774edfc8a6809f40497e9e76381b549d
37jsbean <37jsbean@naver.com>
2024-03-29 16:34:22 +0900

    Fix: 일부 File 주석 처리

--- frontend/src/pages/closet/closet.tsx
+++ frontend/src/pages/closet/closet.tsx
@@ -1,37 +1,37 @@
-// // import useLoginStore from '../../shared/store/useLoginStore';
-// // import useUserStore from '../../shared/store/useUserStore';
+// import useLoginStore from '../../shared/store/useLoginStore';
+// import useUserStore from '../../shared/store/useUserStore';
 
-// // import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // import MyCloset from './myCloset';
 // import MyClosetAnalysis from './myClosetAnalysis';
 
-// const Closet = () => {
-//   // const loginStore = useLoginStore();
-//   // const userStore = useUserStore();
+const Closet = () => {
+  // const loginStore = useLoginStore();
+  // const userStore = useUserStore();
 
-//   return (
-//     <>
-//       <div>
-//         {/* {!loginStore.isLogin ? (
-//           // 로그아웃 상태
-//           <div>
-//             <div>추천</div>
-//             <Link to="/">
-//               <div>홈으로 돌아가기</div>
-//             </Link>
-//           </div>
-//         ) : (
-//           // 로그인 상태 */}
-//         <div>
-//           <div>취향 분석</div>
-//           <MyClosetAnalysis></MyClosetAnalysis>
-//           <div>내 옷장</div>
-//           <MyCloset></MyCloset>
-//         </div>
-//         {/* )} */}
-//       </div>
-//     </>
-//   );
-// };
+  return (
+    <>
+      <div>
+        {/* {!loginStore.isLogin ? (
+          // 로그아웃 상태
+          <div>
+            <div>추천</div>
+            <Link to="/">
+              <div>홈으로 돌아가기</div>
+            </Link>
+          </div>
+        ) : (
+          // 로그인 상태 */}
+        <div>
+          {/* <div>취향 분석</div>
+          <MyClosetAnalysis></MyClosetAnalysis>
+          <div>내 옷장</div>
+          <MyCloset></MyCloset> */}
+        </div>
+        {/* )} */}
+      </div>
+    </>
+  );
+};
 
-// export default Closet;
+export default Closet;
