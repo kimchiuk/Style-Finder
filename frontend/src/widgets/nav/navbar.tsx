fd7a9da9a8650b9b5afd5fa86da57b114317fb84
37jsbean <37jsbean@naver.com>
2024-03-31 15:47:53 +0900

    Fix: merge FE/recommendation <- develop

--- frontend/src/widgets/nav/navbar.tsx
+++ frontend/src/widgets/nav/navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react'; // useEffect 추가
 import useLoginStore from '../../shared/store/use-login-store';
@@ -25,7 +26,7 @@ const Navbar = () => {
   return (
     <div className="navbar flex flex-row h-20 bg-[#161A30]">
       <div className="navbar-start flex items-center justify-center bg-[#F0ECE5] rounded-full m-5 w-36">
-        <div className="logo flex items-center">
+        <div className="flex items-center logo">
           <Link to="/" replace={true}>
             <img className="w-10 h-10" src={logo} alt="" />
           </Link>
@@ -35,7 +36,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="navbar-center flex items-center justify-center flex-grow m-5">
+      <div className="flex items-center justify-center flex-grow m-5 navbar-center">
         <div className="flex justify-between menu menu-sm dropdown-content mt-3 z-[1] p-2 m-5  bg-[#F0ECE5] rounded-full w-1/2">
           <Link to="/coordi" replace={true} className="mr-8">
             코디
@@ -55,7 +56,7 @@ const Navbar = () => {
             로그인
           </Link>
         ) : (
-          <div className="flex flex items-center justify-center">
+          <div className="flex items-center justify-center">
             <button onClick={handleLogout}>로그아웃</button>
           </div>
         )}
